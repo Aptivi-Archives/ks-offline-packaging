8a7a5bfc9572ee29f6f16ec79377571860cc199d
EoflaOE <eoflaoevicecity@gmail.com>
2021-04-30 15:06:10 +0300

    Initial offline commit
    It's still not done yet, so you might see placeholders of Roslyn.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ks-offline-packaging
-Offline packaging for KS
+# Kernel Simulator Offline Packaging
+
+This is a new iteration for Kernel Simulator offline packaging. To initialize the offline package, follow these steps:
+
+1. Clone this repository
+2. Fetch the submodules by running `git submodule update --init`
+3. Run initializeoffline.sh
+
+To build KS offline, use `msbuild` from the Kernel-Simulator directory.
---
+++ initializeoffline.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Run from root directory of linux-offline-packaging-ks
+
+# Script variables
+generate_essentials_tar_xz=false
+
+# Check if user needs to generate Essentials.tar.xz
+if [ "$1" == "--generate-essentials-archive" ]; then
+	generate_essentials_tar_xz=true
+	echo "--generate-essentials-archive invoked"
+fi
+
+# Restore packages
+echo "- Restoring packages..."
+echo "  - cd Kernel-Simulator"
+cd Kernel-Simulator
+echo "  - HOME=`pwd`/nuget msbuild -t:restore"
+HOME=`pwd`/nuget msbuild -t:restore
+if [ "$?" -ne 0 ]; then
+	exit $?
+fi
+
+# Copy dependencies to Kernel-Simulator/deps
+echo "- Copying dependencies to Kernel-Simulator/deps..."
+echo "  - mkdir deps"
+mkdir deps
+echo "  - cp -R ./nuget/.nuget/packages/* ./deps/"
+cp -R ./nuget/.nuget/packages/* ./deps/
+
+# Compressing essential packages
+if [ ${generate_essentials_tar_xz} == true ]; then
+	echo "- Compressing essential packages to Essentials.tar..."
+	echo "  - tar cfv ../Essentials.tar deps"
+	tar cfv ../Essentials.tar deps
+	echo "  - xz -9 ../Essentials.tar"
+	xz -9 ../Essentials.tar
+fi
+
+# Copy NuGet.config for offline use
+echo "- Copying NuGet.config..."
+echo "  - cp ../offline/NuGet.config ."
+cp ../offline/NuGet.config .
+
+# Cleaning up
+echo "- Cleaning up..."
+echo "  - rm -R KSBuild"
+rm -R KSBuild
+echo "  - rm -R obj"
+rm -R obj
+
+echo "- Build using \"msbuild\" from the \"Kernel-Simulator\" directory."
+echo "- For Launchpad PPAs and general Ubuntu package builds, change \"preview\" in \"debian/changelog\" to \"focal\" or any Ubuntu codename."
+echo "- You may want to run \"dch -U\" to sign your custom KS package changelog."
+echo "- Use \"debuild -S -sa\" to build source package for \"sbuild\"."
+echo "- Undo patches once done."
+
